56843d5ee3fffdbcbba109a8bd80a1ead2a2e974
Franzika0 <88448184+Franzika0@users.noreply.github.com>
2024-07-12 19:03:28 +0800

    first commit

---
+++ dist/headShot.js
@@ -0,0 +1,14 @@
+const info = [
+    {
+        name:"ex",
+        image: require("./images/ex.png"),
+        resizeMode: 'contain'
+    },
+    {
+        name:"ex2",
+        image: require("./images/ex2.png"),
+        resizeMode: 'contain'
+    }
+]
+
+export const headShotInfo = info;
